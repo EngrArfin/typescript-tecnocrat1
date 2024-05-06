a989def32fd87a2d4c6fe51bf66300b4123ea3e3
EngrArfin <arfin.cse.green.edu.bd@gmail.com@>
2024-05-06 13:37:53 +0600

    typescript adult or not adult

---
+++ module1/src/1.11.ts
@@ -0,0 +1,16 @@
+{
+    //ternary operator 
+
+    const age: number = 18;
+    
+    if(age >= 18){
+        console.log("adult");
+    }
+    else{
+        console.log("not adult");
+
+    }
+
+    //
+
+}
